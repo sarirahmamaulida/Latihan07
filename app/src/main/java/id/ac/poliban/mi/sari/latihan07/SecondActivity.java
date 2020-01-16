7b7205ef9f93ce12a41cbcbcdc00d6f609c1e255
sarirahmamaulida <sarirahmamaulida@gmail.com>
2020-01-16 09:35:50 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/sari/latihan07/SecondActivity.java
@@ -0,0 +1,19 @@
+package id.ac.poliban.mi.sari.latihan07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //ubah title default pada action bar
+        if (getSupportActionBar()!= null)
+            getSupportActionBar().setTitle("Second Activity");
+
+    }
+}
